00a24eff4f3788078c43896aa120c2ee93f060f3
Lucas <lucasperez.ad47@gmail.com>
2024-11-02 19:22:52 -0300

    screen creadas y barril actualizado

---
+++ lib/presentation/screen/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfinteScrollScreen extends StatelessWidget {
+  static const name = 'infinite_screen';
+
+  const InfinteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/presentation/screen/screen.dart
+++ lib/presentation/screen/screen.dart
@@ -1,3 +1,10 @@
+export 'package:flutter/material.dart';
+export 'package:widget_app/presentation/screen/animated/animated_screen.dart';
+export 'package:widget_app/presentation/screen/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_app/presentation/screen/buttons/buttons_screen.dart';
 export 'package:widget_app/presentation/screen/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screen/home/home_screen.dart';
+export 'package:widget_app/presentation/screen/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widget_app/presentation/screen/progress/progress_screen.dart';
+export 'package:widget_app/presentation/screen/snackbar/snackbar_screen.dart';
+export 'package:widget_app/presentation/screen/ui_controls/ui_controls_screen.dart';
